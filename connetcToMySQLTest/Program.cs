1048203d5f1ffbd90bc061742a96bc91d6448ab2
马嘉远 <944414275@qq.com>
2019-05-08 10:00:19 +0800

    test

--- connetcToMySQLTest/Program.cs
+++ connetcToMySQLTest/Program.cs
@@ -2,13 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using MySql.Data.MySqlClient;
+using System.Configuration;
 
 namespace connetcToMySQLTest
 {
     class Program
     {
+        static MySqlConnection sqlcon;
+        static string conStr; 
+
         static void Main(string[] args)
         {
+            conStr = ConfigurationManager.AppSettings["mysqlConStr"];
+            sqlcon = new MySqlConnection(conStr);
+            try
+            {
+                sqlcon.Open();
+                Console.WriteLine("已连接数据库");
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
         }
     }
 }
